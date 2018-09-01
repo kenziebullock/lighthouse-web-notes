67d71d1b96e1bff274edf0f6b73e711fbd7063b4
Kenzie Bullock <kenzieb_13@shaw.ca>
2018-09-01 17:33:53 +0000

    added week 2 folders

---
+++ week1/w1d5/luhn/practice.js
@@ -0,0 +1,4 @@
+var array1 = [1, 2, 3, 4];
+var reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+console.log(array1.reduce(reducer));
---
+++ week1/w1d5/luhn/test/test.js
@@ -0,0 +1,27 @@
+var assert = require('chai').assert;
+var luhnTest = require('../luhn');
+
+describe("Luhn's test", function() {
+    it("should test if input passes Luhn's test", function() {
+        var number = "123456789";
+        var result = luhnTest(number);
+        assert.isTrue(result);
+    });
+    it("should test if input passes Luhn's test", function() {
+        var number = "123456789";
+        var result = luhnTest(number);
+        assert.isTrue(result);
+    });
+    it("should test if input passes Luhn's test", function() {
+        var number = "123456789";
+        var result = luhnTest(number);
+        assert.isTrue(result);
+    });
+    it("should test if input passes Luhn's test", function() {
+        var number = "123456789";
+        var result = luhnTest(number);
+        assert.isTrue(result);
+    });
+    
+
+})
